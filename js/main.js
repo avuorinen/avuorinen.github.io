f2fad35238f2f7ffaeeba7a2a69221862fbfc1a9
Atte Vuorinen <attevuorinen@gmail.com>
2016-05-26 16:46:38 +0300

    Optimized shrinking.

--- js/main.js
+++ js/main.js
@@ -1,34 +1,62 @@
 function onLoad() {
 
-  onLoad.isSmaller = false;
-  onLoad.isSmallest = false;
+  // Calculate once.
+  var ratio = getEmPixels(document.getElementById("header"))/getEmPixels() * 240; // 240 -> 15em = 16px * 15
+  var shrink = ratio/6, //50
+  shrinker = ratio/2.5, //100
+  shrinkest = ratio/1.5; //160
 
-  window.addEventListener("scroll", function(event){
-    var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-    shrinkOn = 50, //35,40
-    header = document.getElementById("header");
+  onLoad.shrinkLevel = 0;
 
-    if(distanceY / 2 > shrinkOn ) {
-      if(!onLoad.isSmallest) {
-        onLoad.isSmallest = true;
-        header.className += " smallest";
+  window.addEventListener("scroll", function(event) {
+
+    var header = document.getElementById("header");
+    var distanceY = window.pageYOffset || document.documentElement.scrollTop;
+
+    function addClass(className) {
+      header.className += " " + className;
+    }
+
+    function removeClass(className) {
+      function escapeRegExp(stringToGoIntoTheRegex) {
+          return stringToGoIntoTheRegex.replace(/(?:^|\s)\\^$(?!\S)/g, '\\$&');
       }
-    }
-    else if(onLoad.isSmallest) {
-      onLoad.isSmallest = false;
-      header.className = header.className.replace( /(?:^|\s)smallest(?!\S)/g , '' )
+
+      var regex = new RegExp(escapeRegExp(className));
+
+      header.className = header.className.replace(regex , '' );
     }
 
-    if (distanceY > shrinkOn) {
-
-      if(!onLoad.isSmaller) {
-        onLoad.isSmaller = true;
-        header.className += " smaller";
+    if(distanceY > shrinkest ) {
+      if(onLoad.shrinkLevel == 2) {
+        onLoad.shrinkLevel++;
+        addClass("smallest");
       }
-    } else if(distanceY < shrinkOn && onLoad.isSmaller) {
-      onLoad.isSmaller = false;
-      header.className = header.className.replace( /(?:^|\s)smaller(?!\S)/g , '' )
+    } else if(onLoad.shrinkLevel == 3) {
+      onLoad.shrinkLevel--;
+      removeClass("smallest");
     }
+
+    if (distanceY > shrinker) {
+      if(onLoad.shrinkLevel == 1) {
+        onLoad.shrinkLevel++;
+        addClass("smaller");
+      }
+    } else if(onLoad.shrinkLevel == 2) {
+      onLoad.shrinkLevel--;
+      removeClass("smaller");
+    }
+
+    if (distanceY > shrink) {
+      if(onLoad.shrinkLevel == 0) {
+        onLoad.shrinkLevel++;
+        addClass("small");
+      }
+    } else if(onLoad.shrinkLevel == 1) {
+      onLoad.shrinkLevel--;
+      removeClass("small");
+    }
+
   });
 }
 window.onload = onLoad();
